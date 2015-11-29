e75cc5a658ab4f87aa1793420dabe7138b275983
Ivan Sorokin <vanyacpp@gmail.com>
2015-11-30 01:44:51 +0300

    extract echo_server into a separate file

---
+++ echo_server.h
@@ -0,0 +1,36 @@
+#ifndef ECHO_SERVER_H
+#define ECHO_SERVER_H
+
+#include <map>
+#include "socket.h"
+
+struct echo_server
+{
+    struct connection
+    {
+        connection(echo_server* parent);
+        void update();
+
+    private:
+        echo_server* parent;
+        client_socket socket;
+        size_t start_offset;
+        size_t end_offset;
+        char buf[1500];
+    };
+
+    echo_server(epoll& ep);
+
+    echo_server(epoll& ep, ipv4_endpoint const& local_endpoint);
+
+    ipv4_endpoint local_endpoint() const;
+
+private:
+    void on_new_connection();
+
+private:
+    server_socket ss;
+    std::map<connection*, std::unique_ptr<connection>> connections;
+};
+
+#endif // ECHO_SERVER_H
