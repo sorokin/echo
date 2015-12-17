6da76a01f2ec3603ae0de2b7d8dfd3b8b8a799d5
Dmitry Kurkin <Kurkin@users.noreply.github.com>
2015-12-18 02:41:35 +0300

    Update main_echo_server.cpp

--- main_echo_server.cpp
+++ main_echo_server.cpp
@@ -9,8 +9,8 @@
 
 int main()
 {
-//    try
-//    {
+    try
+    {
         sysapi::epoll ep;
         echo_server echo_server(ep, ipv4_endpoint(0, ipv4_address::any()));
 
@@ -18,17 +18,17 @@ int main()
         std::cout << "bound to " << echo_server_endpoint << std::endl;
 
         ep.run();
-//    }
-//    catch (std::exception const& e)
-//    {
-//        std::cerr << "error: " << e.what() << std::endl;
-//        return EXIT_FAILURE;
-//    }
-//    catch (...)
-//    {
-//        std::cerr << "unknown exception in main" << std::endl;
-//        return EXIT_FAILURE;
-//    }
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << "error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        std::cerr << "unknown exception in main" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
